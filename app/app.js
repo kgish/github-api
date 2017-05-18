dcf330394ed44c735b4abc18dadb9796d03add6f
Kiffin Gish <kiffin.gish@planet.nl>
2017-05-18 10:43:08 +0200

    Replaced default welcome page with own landing index page.

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@ import config from './config/environment';
 
 let App;
 
-Ember.MODEL_FACTORY_INJECTIONS = true;
+// No longer needed.
+// Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
